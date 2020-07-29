08d026139787fc15f96fd42295d488d438804a25
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-29 19:54:09 +0100

    Added cards layout into dashboards

--- src/components/customer/customerDashboard.jsx
+++ src/components/customer/customerDashboard.jsx
@@ -9,9 +9,63 @@ class CustomerDashboard extends Component {
       <React.Fragment>
         <h2>Hello {user.firstName} | customer</h2>
         <h3>Some data about your account:</h3>
-        <Link className="btn btn-primary" to="/customer/address">
-          Add Address
-        </Link>
+
+        <div className="container">
+          <div className="row mt-5 mb-5">
+            <div className="col-sm-6">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Do you have a faulty unit?</h5>
+                  <p className="card-text">
+                    You can easily create a new ticket here.
+                  </p>
+                  <Link to="/customer/ticket/new" className="btn btn-secondary">
+                    Create a Ticket
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="card ">
+                <div className="card-body">
+                  <h5 className="card-title">More delivery address?</h5>
+                  <p className="card-text">
+                    Do not worry, you can add your current address here.
+                  </p>
+                  <Link to="/customer/address" className="btn btn-secondary">
+                    Add Address
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="card ">
+                <div className="card-body">
+                  <h5 className="card-title">Review Tickets</h5>
+                  <p className="card-text">You can review your all tickets.</p>
+                  <Link to="/customer/tickets" className="btn btn-secondary">
+                    Review Tickets
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="card ">
+                <div className="card-body">
+                  <h5 className="card-title">Your Address</h5>
+                  <p className="card-text">
+                    You can check your current address.
+                  </p>
+                  <Link to="/customer/tickets" className="btn btn-secondary">
+                    Your Address
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
