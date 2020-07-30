0ac7fba45b18cdeb0d09d5af318ba0834cc8da9a
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-30 15:47:31 +0100

    Removed redundant component

--- src/components/addressForm.jsx
+++ src/components/addressForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { ToastContainer } from "react-toastify";
 import Joi from "joi-browser";
 import Form from "./common/form";
