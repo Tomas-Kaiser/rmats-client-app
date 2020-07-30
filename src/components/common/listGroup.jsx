2c82d43b682b5191cf1fb043e3ce9b69d74cbe0f
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-30 20:04:15 +0100

    Implemented filters tickets based on their status

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ListGroup = props => {
+  const { items, onItemSelect, selectedItem } = props;
+
+  console.log("Items", items);
+
+  return (
+    <ul className="list-group mt-5">
+      {items.map((item, index) => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={index}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListGroup;
--- src/components/customer/customerTickets.jsx
+++ src/components/customer/customerTickets.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import ListGroup from "../common/listGroup";
 import { getTicketsByCustomer } from "../../services/ticketService";
 import { getReplacementUnitByTicketId } from "./../../services/replacementService";
 
 class CustomerTickets extends Component {
   state = {
-    tickets: []
+    tickets: [],
+    status: ["All Tickets", "Open", "Closed"]
   };
 
   async componentDidMount() {
@@ -29,6 +31,7 @@ class CustomerTickets extends Component {
           }
         }
       }
+      console.log("updatedTickets", updatedTickets);
       this.setState({ tickets: updatedTickets });
     } catch (error) {
       if (error.response && error.response.status === 404) {
@@ -36,57 +39,86 @@ class CustomerTickets extends Component {
       }
     }
   }
-  render() {
-    return (
-      <React.Fragment>
-        <h1 className="mt-4">Ticket{this.state.tickets === 1 ? "" : "s"}</h1>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Ticket Id</th>
-              <th scope="col">Date</th>
-              <th scope="col">Comment</th>
-              <th scope="col"></th>
-              <th scope="col">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.tickets.map(ticket => (
-              <tr key={ticket.id}>
-                <th scope="row">{ticket.id}</th>
-                <td>{ticket.raiseDate}</td>
-                <td>{ticket.comment}</td>
-                <td>
-                  <Link
-                    to={`/ticket/${ticket.id}`}
-                    className="btn btn-secondary"
-                  >
-                    Detail
-                  </Link>
-                </td>
-                {ticket.replacement.length !== 0 && (
-                  <td>
-                    {ticket.replacement[0].status.toLowerCase().trim() ===
-                    "delivered"
-                      ? "Closed"
-                      : "Open"}
-                  </td>
-                )}
-                {ticket.replacement.length === 0 && <td>TBD</td>}
-              </tr>
-            ))}
-          </tbody>
-        </table>
 
-        {this.state.tickets.length === 0 && (
-          <div className="container text-center">
-            <p className="text-info">No tickets created yet.</p>
-            <Link to="/customer/ticket/new" className="btn btn-secondary">
-              Create Ticket
-            </Link>
-          </div>
-        )}
-      </React.Fragment>
+  handleStatusSelect = status => {
+    this.setState({ selectedStatus: status });
+  };
+
+  render() {
+    const { selectedStatus } = this.state;
+
+    console.log("selected STAATUS: ", selectedStatus);
+
+    const filtered =
+      selectedStatus && !(selectedStatus === "All Tickets")
+        ? this.state.tickets.filter(
+            t =>
+              (t.replacement[0].status.toLowerCase().trim() === "delivered"
+                ? "Closed"
+                : "Open") === selectedStatus
+          )
+        : this.state.tickets;
+
+    console.log("Filteeeer: ", filtered);
+
+    return (
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={this.state.status}
+            selectedItem={this.state.selectedStatus}
+            onItemSelect={this.handleStatusSelect}
+          />
+        </div>
+        <div className="col">
+          <h1 className="mt-4">Ticket{this.state.tickets === 1 ? "" : "s"}</h1>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Ticket Id</th>
+                <th scope="col">Date</th>
+                <th scope="col">Comment</th>
+                <th scope="col"></th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filtered.map(ticket => (
+                <tr key={ticket.id}>
+                  <th scope="row">{ticket.id}</th>
+                  <td>{ticket.raiseDate}</td>
+                  <td>{ticket.comment}</td>
+                  <td>
+                    <Link
+                      to={`/ticket/${ticket.id}`}
+                      className="btn btn-secondary"
+                    >
+                      Detail
+                    </Link>
+                  </td>
+                  {ticket.replacement.length !== 0 && (
+                    <td>
+                      {ticket.replacement[0].status.toLowerCase().trim() ===
+                      "delivered"
+                        ? "Closed"
+                        : "Open"}
+                    </td>
+                  )}
+                  {ticket.replacement.length === 0 && <td>TBD</td>}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {this.state.tickets.length === 0 && (
+            <div className="container text-center">
+              <p className="text-info">No tickets created yet.</p>
+              <Link to="/customer/ticket/new" className="btn btn-secondary">
+                Create Ticket
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     );
   }
 }
