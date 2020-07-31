a4ec0eb3323f62ec22f3de4110f42fec34840c65
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-31 16:08:07 +0100

    Added style into home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <h1>This is home page</h1>
+        <div className="jumbotron mt-5">
+          <h1 className="display-4">
+            Welcome to our service RMA Ticket System
+          </h1>
+          <p className="lead">This is your helper to manage your RMA easily!</p>
+          <hr className="my-4" />
+          <p className="lead">
+            If you do not have an account yet, please do so below:
+          </p>
+          <Link to="register" className="btn btn-primary">
+            Register
+          </Link>
+        </div>
       </React.Fragment>
     );
   }
