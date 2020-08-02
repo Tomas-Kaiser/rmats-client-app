86c165c8bc828a76b55b401361403952432fb68a
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-08-02 16:45:29 +0100

    Removed MemoryRouter from app.js and placed it into App test instead

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
